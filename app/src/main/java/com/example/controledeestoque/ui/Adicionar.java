6f04550383ccb8b482c8087e22b19490cea3ec72
joaospode <joaospode.contato@gmail.com>
2025-07-21 15:05:16 -0300

    Pronto para uso, V1

--- app/src/main/java/com/example/controledeestoque/ui/Adicionar.java
+++ app/src/main/java/com/example/controledeestoque/ui/Adicionar.java
@@ -2,7 +2,6 @@ package com.example.controledeestoque.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.controledeestoque.R;
